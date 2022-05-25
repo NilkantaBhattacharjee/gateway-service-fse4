eb01a5b6d7372410e8117029933d90a4970a6ed1
Riddhi Sarkar <cogjava404@ctsjava080.iiht.tech>
2022-05-25 16:12:48 +0530

    Added support for Spring Cloud Gateway
    Change-Id: I2a80c62c1ce4c2d236e34d757100b228500bd9b7

--- src/main/java/com/cts/skilltracker/GatewayServiceApplication.java
+++ src/main/java/com/cts/skilltracker/GatewayServiceApplication.java
@@ -2,15 +2,16 @@ package com.cts.skilltracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-@EnableZuulProxy //Acts as reverse proxy, forwarding requests to other services based on routes.
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+	
+	
 
 }
